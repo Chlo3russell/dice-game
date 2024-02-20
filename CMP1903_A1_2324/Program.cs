8611bbcff638c5fcce03bf957fbd63a4e0e708de
Chloe Russell <chloerussell2017@gmail.com>
2024-02-20 20:17:38 +0000

    Finished the Game class and added the testing class

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -14,6 +14,15 @@ namespace CMP1903_A1_2324
             Game newGame = new Game();
             // Using the method DiceRolling to roll the "dice"
             newGame.DiceRolling();
+            // Using the method KeepRolling to create a continuous rolling loop
+            newGame.KeepRolling();
+            // Tells you how many of each no was rolled
+            newGame.DisplayAmounts();
+
+            // Testing the two classes Game and Die
+            Testing test1 = new Testing();
+            test1.DieTesting();
+            test1.GameTesting();
 
         }
     }
