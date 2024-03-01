8bbb3aaadaf73ad6907900a282dcadafe0adcc3d
Chloe Russell <109138712+Chlo3russell@users.noreply.github.com>
2024-03-01 12:57:30 +0000

    Revert "Finished the Game class and added the testing class"

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -15,20 +15,13 @@ namespace CMP1903_A1_2324
         private int _dieTotal {  get; set; }
         private float _dieAverage { get; set; }
 
-        public int _noOf1s = 0;
-        public int _noOf2s = 0;
-        public int _noOf3s = 0;
-        public int _noOf4s = 0;
-        public int _noOf5s = 0;
-        public int _noOf6s = 0;
-
         /// <summary>
         /// The method DieStats creates 3 die objects, uses the roll method and sums the total of the die.
         /// It then asks if you want to contimue rolling, and continues creating new objects, and totalling the total amount rolled
         /// </summary>
 
         // Method
-        public int DiceRolling()
+        public void DiceRolling()
         {
             // while 3 or less die have been created it continues to create and "Roll" new die objects
             while (_diceRolls != 3)
@@ -36,19 +29,12 @@ namespace CMP1903_A1_2324
                 _diceRolls++; // Incrementing the counter by one 
                 Die newDice = new Die(); // Instantiating the object
                 Console.WriteLine("Dice {0} is {1}", _diceRolls, _diceValue = newDice.Roll()); // Outputting the number of dice rolled and the roll value
-                Amounts();
                 _dieTotal += _diceValue; // Adding the roll to the total
             }
-            // Displaying the the total of the three dice
+
             Console.WriteLine("This is the total of your three dice rolls {0}" , _dieTotal);
-
-            return _dieTotal;
-        }
-
-        public void KeepRolling()
-        {
             Console.WriteLine("------------------------------- \nDo you want to roll again, enter no to exit the loop?");
-            // Defining a bool to create a condition for the while loop
+
             bool continueToRoll = true;
             string answer = Console.ReadLine();
 
@@ -63,7 +49,6 @@ namespace CMP1903_A1_2324
                 //Creating a new object again
                 Die newDice = new Die();
                 Console.WriteLine("Dice {0} is {1}", _diceRolls, _diceValue = newDice.Roll());
-                Amounts();
                 // Die statistics
                 _dieTotal += _diceValue;
                 _dieAverage = _dieTotal / _diceRolls;
@@ -72,42 +57,9 @@ namespace CMP1903_A1_2324
                 answer = Console.ReadLine();
                 if (answer.ToLower() == "no")
                 {
-                    continueToRoll = false;
-                }
+                    continueToRoll = false;                 
+                }           
             }
-        }
-
-        public void Amounts()
-        {
-            switch (_diceValue)
-            {
-                case 1:
-                    _noOf1s++;
-                    break;
-                case 2:
-                    _noOf2s++;
-                    break;
-                case 3:
-                    _noOf3s++;
-                    break;
-                case 4:
-                    _noOf4s++;
-                    break;
-                case 5:
-                    _noOf5s++;
-                    break;
-                case 6:
-                    _noOf6s++;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void DisplayAmounts()
-        {
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("Amount of 1s' {0}\nAmount of 2s' {1}\nAmount of 3s' {2}\nAmount of 4s' {3}\nAmount of 5s' {4}\nAmount of 6s' {5}", _noOf1s, _noOf2s, _noOf3s, _noOf4s, _noOf5s, _noOf6s);
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,33 +16,7 @@ namespace CMP1903_A1_2324
          */
 
         //Method
-        public int DieTesting()
-        {
-            Die dice1 = new Die();
-            int die1value = dice1.Roll();
-            Die dice2 = new Die();
-            int die2value = dice2.Roll();
-            Die dice3 = new Die();
-            int dice3value = dice3.Roll();
-            int dieTotal = die1value + die2value + dice3value;
 
-            Debug.Assert(die1value <=6 && die1value >=1, "The dice rolled higher or lower than a 6 sided die can roll");
-            //Debug.Assert(dieTotal >= 18 && dieTotal <= 3, "The total of the dies is outside the logical range");
-
-            return die1value;
-        }
-
-        public int GameTesting()
-        {
-            Game testGame = new Game();
-            Console.WriteLine("-------------------------------");
-            int testGameTotal = testGame.DiceRolling();
-
-            Debug.Assert(testGameTotal <= 18 && testGameTotal >= 3, "The total of the dies is outside the logical range");
-
-            return testGameTotal;
-        }
-
-
+       
     }
 }
